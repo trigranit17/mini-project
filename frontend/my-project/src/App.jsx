6b8ab42df84ab837f7e86636f5269ef22e5e168f
Tabah Granit Tri Aziz <112501312+trigranit17@users.noreply.github.com>
2024-03-06 00:05:03 +0700

    Update App.jsx

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,16 +1,23 @@
-// src/App.js
 import React from 'react';
-// import Login from './components/Login';
+import Login from './components/Login';
 import AdminDashboard from './components/AdminDashboard';
+import QuizDetails from './components/QuizDetails'; 
+import QuizForm from './components/QuizForm'; 
+import ParticipantAnswers from './components/ParticipantAnswers'; 
 
 function App() {
   return (
-    <div>
-      <h1>Quiz App</h1>
-      {/* <Login /> */}
-      <AdminDashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/quiz/:quizId" element={<QuizDetails />} />
+        <Route path="/admin/quiz/:quizId/answers" element={<ParticipantAnswers />} />
+        <Route path="/admin/quiz-form" element={<QuizForm />} />
+        <Route path="/admin/quiz-form/:quizId" element={<QuizForm />} />
+      </Routes>
+    </Router>
   );
-}
+} 
 
 export default App;
